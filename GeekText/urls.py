edc6af2863fc4923dabd25e2c74eebdce564908f
Edward Basulto <48189137+Ebasu004@users.noreply.github.com>
2021-05-24 00:17:51 -0400

    Add files via upload

---
+++ GeekText/urls.py
@@ -0,0 +1,47 @@
+"""GeekText URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic.base import RedirectView
+from django.contrib.auth import views as auth_views
+from users import views as user_views
+from . import views
+
+urlpatterns = [
+                  url(r'^admin/', admin.site.urls, name='admin'),
+                  url(r'^home/', include('Browse.urls'), name='home'),
+                  url(r'^wishlist/', views.wishlists, name='wishlist'),
+                  url(r'^browse/', include('Browse.urls'), name='browse'),
+                  url(r'^cart/$', views.cart, name='cart'),
+                  url(r'^checkout/$', views.checkout, name='checkout'),
+                  url(r'^$', RedirectView.as_view(url='home', permanent=True), name='index'),
+                  path('author/<str:book_author>/', include('Author.urls'), name='author'),
+                  path('browse/details/<int:book_id>/', include('BookDetails.urls'), name='details'),
+                  url(r'signup/$', user_views.register, name='register'),
+                  url(r'login/$',
+                      auth_views.LoginView.as_view(template_name='users/login.html', redirect_authenticated_user=True),
+                      name='login'),
+                  # class based view
+                  url(r'logout/$', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+                  # class based view
+                  url(r'profile/$', user_views.profile, name='profile'),
+                  url(r'password_change/$', user_views.change_password, name='change_password'),
+                  url(r'payment/$', user_views.payment_info, name='payment_info'),
+                  url(r'shipping/$', user_views.shipping_info, name='shipping_info'),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ GeekText/views.py
@@ -0,0 +1,55 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.utils.html import escape
+
+import Browse
+from Browse.models import Book
+from Browse.views import browse
+
+
+def homepage(request):
+    return render(request, "homepage.html")
+
+
+def wishlists(request):
+    title = request.GET.get('title')
+    author = request.GET.get('author')
+
+    if title and author:
+        return HttpResponse(escape(repr(title + author)))
+
+    return render(request, "wishlist.html")
+
+
+def cart(request):
+    args = {}
+    books: [Book] = []
+    saved: [Book] = []
+    if request.user.is_authenticated:
+        for book_id in request.user.shoppingcart.shopping_cart.split():
+            books += Book.objects.filter(pk=book_id)
+        for book_id in request.user.shoppingcart.save_for_later.split():
+            saved += Book.objects.filter(pk=book_id)
+    args['shopping_cart'] = books
+    args['saved_books'] = saved
+    return render(request, "shoppingcart.html", args)
+
+
+def checkout(request):
+    args = {}
+    shopping_cart: [Book] = []
+    cart_total: int = 0
+    if request.user.is_authenticated:
+        if request.user.shoppingcart is not None:
+            for book_id in request.user.shoppingcart.shopping_cart.split():
+                shopping_cart += Book.objects.filter(pk=book_id)
+    for book in shopping_cart:
+        cart_total += book.price
+    args['cart_size'] = len(shopping_cart)
+    args['shopping_cart'] = shopping_cart
+    args['total_price'] = cart_total
+    return render(request, "checkout.html", args)
+
+
+def browse(request):
+    return Browse.views.browse(request)
